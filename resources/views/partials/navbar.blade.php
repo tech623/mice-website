f7c620b410f46d58332751232c047b534dade922
Sanjay <tech@darenetwork.com>
2026-07-31 07:46:40 +0000

    Header: match nav link styling, add 10 Years badge, adjust service-navbar offset

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1728,10 +1728,11 @@
 
 </style>
 <nav class="navbar navbar-expand-lg navbar-light fixed-top main-navigation row" style="border-bottom: 1px solid #F1F2ED;">
-    <div class="col-md-3 col-8">
+    <div class="col-md-3 col-8 d-flex align-items-center">
         <a class="navbar-brand navbarLogo" href="{{route('home')}}">
-            <img src="{{asset('images/newLogo.svg')}}" class="d-inline-block align-top c-logo" alt="" width="70%">
+            <img src="{{asset('images/newLogo.svg')}}" class="d-inline-block align-top c-logo" alt="" width="100%">
         </a>
+        <img src="{{asset('images/IMG-20260409-WA0014.jpg')}}" alt="10 Years of Excellence" style="height:80px; width:auto; margin-left:15px;">
     </div>
     <!--<div class="col-md-6 menu-search-bar d-none d-lg-block d-xl-block d-xl-none">
         <?php $states = App\Models\Property::getStates();
@@ -1898,6 +1899,18 @@
                 <li class="nav-item {{ request()->is("/*") ? "active" : "" }}" style="margin-right:25px">
                     <a class="nav-link" href="{{route('home')}}">Home <span class="sr-only">(current)</span></a>
                 </li>
+                <li class="nav-item {{ request()->is('why-mice*') ? 'active' : '' }}" style="margin-right:25px">
+    <a class="nav-link" href="{{route('why-mice')}}">About Us</a>
+</li>
+<li class="nav-item {{ request()->is('blogs*') ? 'active' : '' }}" style="margin-right:25px">
+    <a class="nav-link" href="{{route('blogs.index',__('pagination.defaultPage'))}}">Blog</a>
+</li>
+<li class="nav-item {{ request()->is('contact-us*') ? 'active' : '' }}">
+    <a class="nav-link" href="{{route('contact-us')}}">Contact Us</a>
+</li>
+                
+                
+            <!--
                 <li class="nav-item" style="border:1px solid #F1F2ED; border-radius:100px">
                     <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         <svg xmlns="http://www.w3.org/2000/svg" class="mr-2" width="20" height="20" viewBox="0 0 20 20" fill="none">
@@ -1907,7 +1920,7 @@
                             <path fill-rule="evenodd" clip-rule="evenodd" d="M12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 12 2ZM12 5C13.66 5 15 6.34 15 8C15 9.66 13.66 11 12 11C10.34 11 9 9.66 9 8C9 6.34 10.34 5 12 5ZM6 15.98C7.29 17.92 9.5 19.2 12 19.2C14.5 19.2 16.71 17.92 18 15.98C17.97 13.99 13.99 12.9 12 12.9C10 12.9 6.03 13.99 6 15.98Z" fill="#323232" />
                         </svg>
                     </a>
-                    <div class="dropdown-menu menu-bar">
+                  <div class="dropdown-menu menu-bar">
                         @if(Auth::check())
                             <a href="#" class="dropdown-item">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="mr-2" width="24" height="24" viewBox="0 0 24 24" fill="none">
@@ -1918,8 +1931,8 @@
                              <a class="dropdown-item" href="{{route('web-login.profile')}}">My Profile</a>
                              <div class="dropdown-divider"></div>
                         @else
-                           <!-- <a class="dropdown-item" href="javascript:void(0);"  data-toggle="modal" data-target="#registerModel">Sign Up</a>
-                            <a class="dropdown-item" href="javascript:void(0);" data-toggle="modal" data-target="#loginModel">Login</a> -->
+                           <a class="dropdown-item" href="javascript:void(0);"  data-toggle="modal" data-target="#registerModel">Sign Up</a>
+                            <a class="dropdown-item" href="javascript:void(0);" data-toggle="modal" data-target="#loginModel">Login</a> 
                         @endif
                         <a class="dropdown-item" href="{{route('why-mice')}}">About Us</a>
                         <a class="dropdown-item" href="{{route('blogs.index',__('pagination.defaultPage'))}}">Blog</a>
@@ -1937,7 +1950,8 @@
                         <a class="dropdown-item btn contactBtn" href="{{route('contact-us')}}">Contact Us</a>
                     </div>
                 </li>
-            </ul>
+            </ul> -->
+            
         </div>
         <ul class="navbar-nav ml-auto d-flex d-sm-none">
             <li class="nav-item" style="border:1px solid #F1F2ED; border-radius:100px">
