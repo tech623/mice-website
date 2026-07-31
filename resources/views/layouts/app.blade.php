90b430ca47c7bd9417893402ce8ff1a336cd3934
Sanjay <tech@darenetwork.com>
2026-07-31 10:45:53 +0000

    Remove profile icon from mobile menu button

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -278,9 +278,10 @@
                     @if(Auth::check())
                         <li class="nav-item">
                             <a href="#" class="nav-link">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="mr-2" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                               <!-- <svg xmlns="http://www.w3.org/2000/svg" class="mr-2" width="24" height="24" viewBox="0 0 24 24" fill="none">
                                     <path fill-rule="evenodd" clip-rule="evenodd" d="M12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 12 2ZM12 5C13.66 5 15 6.34 15 8C15 9.66 13.66 11 12 11C10.34 11 9 9.66 9 8C9 6.34 10.34 5 12 5ZM6 15.98C7.29 17.92 9.5 19.2 12 19.2C14.5 19.2 16.71 17.92 18 15.98C17.97 13.99 13.99 12.9 12 12.9C10 12.9 6.03 13.99 6 15.98Z" fill="#323232"/>
-                                </svg> <span style="font-weight:700">{{Auth::user()->name}}</span>
+                                </svg> <span style="font-weight:700">{{Auth::user()->name}}</span> 
+                                -->
                             </a>
                         </li>
                         <li class="nav-item">
@@ -295,12 +296,13 @@
                         </li>
                         <li class="nav-item" style="border-bottom: 1px solid #dee2e6"></li>
                     @else
-                        <li class="nav-item">
+                       <!-- <li class="nav-item">
                             <a class="nav-link" href="javascript:void(0);" data-toggle="modal" data-target="#registerModel">Sign Up</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="javascript:void(0);" data-toggle="modal" data-target="#loginModel">Login</a>
-                        </li>
+                        </li> 
+                        -->
                         <li class="nav-item" style="border-bottom: 1px solid #dee2e6"></li>
                     @endif
 
