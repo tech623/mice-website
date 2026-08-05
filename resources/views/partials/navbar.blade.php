a82b6e8a7d76e81df4390fd0f0b97ba6f275ca71
Sanjay <tech@darenetwork.com>
2026-08-05 11:32:56 +0000

    Mobile fixes: padding, remove duplicate hamburger, menu overflow, contact page redesign

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1916,7 +1916,7 @@
     </div>
 
     <ul class="navbar-nav d-flex d-sm-none" style="margin-left:auto;">
-        <li class="nav-item" style="border:1px solid #F1F2ED; border-radius:100px">
+       <li class="nav-item">
             <a class="nav-link" href="javascript:void(0)" id="openNavBtn" style="text-align:center">
                 <svg xmlns="http://www.w3.org/2000/svg" class="mr-2" width="20" height="20" viewBox="0 0 20 20" fill="none">
                     <path fill-rule="evenodd" clip-rule="evenodd" d="M3.33333 6.66667C2.875 6.66667 2.5 6.29167 2.5 5.83333C2.5 5.375 2.875 5 3.33333 5H16.6667C17.125 5 17.5 5.375 17.5 5.83333C17.5 6.29167 17.125 6.66667 16.6667 6.66667H3.33333ZM3.33333 10.8333H16.6667C17.125 10.8333 17.5 10.4583 17.5 10C17.5 9.54167 17.125 9.16667 16.6667 9.16667H3.33333C2.875 9.16667 2.5 9.54167 2.5 10C2.5 10.4583 2.875 10.8333 3.33333 10.8333ZM3.33333 15H16.6667C17.125 15 17.5 14.625 17.5 14.1667C17.5 13.7083 17.125 13.3333 16.6667 13.3333H3.33333C2.875 13.3333 2.5 13.7083 2.5 14.1667C2.5 14.625 2.875 15 3.33333 15Z" fill="#323232" />
@@ -1982,9 +1982,10 @@
                         <a class="dropdown-item btn contactBtn" href="{{route('contact-us')}}">Contact Us</a>
                     </div>
                 </li>
-            </ul> -->
+            </ul> 
             
         </div>
+        <!--
         <ul class="navbar-nav ml-auto d-flex d-sm-none">
             <li class="nav-item" style="border:1px solid #F1F2ED; border-radius:100px">
                 <a class="nav-link" href="javascript:void(0)" id="openNavBtn" style="text-align:center">
@@ -1996,9 +1997,12 @@
                         <path fill-rule="evenodd" clip-rule="evenodd" d="M12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 12 2ZM12 5C13.66 5 15 6.34 15 8C15 9.66 13.66 11 12 11C10.34 11 9 9.66 9 8C9 6.34 10.34 5 12 5ZM6 15.98C7.29 17.92 9.5 19.2 12 19.2C14.5 19.2 16.71 17.92 18 15.98C17.97 13.99 13.99 12.9 12 12.9C10 12.9 6.03 13.99 6 15.98Z" fill="#323232" />
                     </svg>
                     -->
+                    
                 </a>
             </li>
-        </ul>
+        </ul> 
+    
+        
 
     </div>
 </nav>
